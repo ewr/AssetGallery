3b0486290ca4dfadcab1f96d3bf390430ea3d70d
Eric Richardson <e@ericrichardson.com>
2012-01-02 14:11:19 -0800

    Make SetAssets dependent on sets; tweak to bdv4cms call

--- app/models/asset_gallery/set.rb
+++ app/models/asset_gallery/set.rb
@@ -1,6 +1,7 @@
 module AssetGallery
   class Set < ActiveRecord::Base
-    has_many :set_assets
+    has_many :set_assets, :dependent => :destroy
+    
     validate :title, :presence => true
     
     scope :published, where(:is_published => true).order("created_at desc")
--- db/migrate/20120101002704_create_asset_gallery_sets.rb
+++ db/migrate/20120101002704_create_asset_gallery_sets.rb
@@ -2,7 +2,7 @@ class CreateAssetGallerySets < ActiveRecord::Migration
   def change
     create_table :asset_gallery_sets do |t|
       t.string :title, :null => false
-      t.text :description
+      t.text :description, :notes
       t.boolean :is_published, :default => false
       t.boolean :is_public, :default => true
       t.string :password
